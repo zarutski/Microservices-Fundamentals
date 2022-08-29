d847703628fdb79390c3cb030e1321a7a75f2ffa
Maskim Zarutski <maksim.zarutski@ukg.com>
2022-08-30 00:48:32 +0300

    [task-7] : logging and monitoring implementation

--- storage/src/main/java/com/epam/learn/storage/service/StorageService.java
+++ storage/src/main/java/com/epam/learn/storage/service/StorageService.java
@@ -2,12 +2,14 @@ package com.epam.learn.storage.service;
 
 import com.epam.learn.storage.domain.Storage;
 import com.epam.learn.storage.repository.StorageRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @Transactional
 public class StorageService {
@@ -19,6 +21,7 @@ public class StorageService {
     }
 
     public Storage create(Storage storage) {
+        log.info("Storage input while creation: " + storage);
         return storageRepository.save(storage);
     }
 
@@ -27,6 +30,7 @@ public class StorageService {
     }
 
     public List<Storage> findAll() {
+        log.info("Loading available storages");
         return storageRepository.findAll();
     }
 
